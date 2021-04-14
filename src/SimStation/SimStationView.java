b94b1a080ba90385d21905cae4ab8d23b8584473
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2021-04-14 14:43:29 -0700

    Sim Station View

--- src/SimStation/SimStationView.java
+++ src/SimStation/SimStationView.java
@@ -1,11 +1,29 @@
 package SimStation;
 
-import mvc.*;
+import mvc.Model;
+import mvc.View;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class SimStationView extends View {
     private Simulation station;
-    public SimStationView(Model model) {
-        super(model);
-        station = (Simulation) model;
+    private ArrayList<Agent> viewList;
+
+
+    public SimStationView(Model station) {
+        super(station);
+
     }
+
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+        Simulation station = (Simulation) model;
+        viewList = station.getList();
+        for (Agent agent : viewList) {
+            gc.fillRect(7, 7, agent.getXc(), agent.getYc());
+        }
+
+    }
+
 }

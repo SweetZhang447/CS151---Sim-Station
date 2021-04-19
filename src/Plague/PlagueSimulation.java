04eb227c2875f7349cb6070a31398c97a252c9d5
Hoc Can Huynh <canhuynh1998@gmail.com>
2021-04-18 20:33:19 -0700

    Modified Plague

--- src/Plague/PlagueSimulation.java
+++ src/Plague/PlagueSimulation.java
@@ -9,36 +9,33 @@ import mvc.View;
 import java.util.HashMap;
 import java.util.Map;
 
-class Person extends Agent {
-    public static int RESISTANCE = 5; // % chance of resisting infection
-    private final int speed = 1;
-    private boolean infected = false;
-
-    public Person() {
-        super();
-        headTo = Heading.random();
-
-    }
-
-    public boolean isInfected() {
-        return infected;
-    }
-
-    public void setInfected(boolean infected) {
-        this.infected = infected;
-    }
-
-    public void update() {
-        Plague.Person neighbor = (Plague.Person) this.world.getNeighbor(this, 10);    //10 is current a random number, we can change it later on
-
-        if (neighbor != null && neighbor.isInfected() && Math.random() * 100 > RESISTANCE) {
-            this.setInfected(true);
-        }
-
-        move(speed);
-    }
-
-}
+//class Person extends Agent {
+//    public static int RESISTANCE = 2; // % chance of resisting infection
+//    private final int speed = 1;
+//    private boolean infected = false;
+//
+//    public Person() {
+//        super();
+//        headTo = Heading.random();
+//    }
+//
+//    public boolean isInfected() {
+//        return infected;
+//    }
+//
+//    public void setInfected(boolean infected) {
+//        this.infected = infected;
+//    }
+//
+//    public void update() {
+//        Plague.Person neighbor = (Plague.Person) this.world.getNeighbor(this, 10);    //10 is current a random number, we can change it later on
+//        double randomValue = Math.random() * 100;
+//        if (neighbor != null && neighbor.isInfected() && randomValue > RESISTANCE) {
+//            this.setInfected(true);
+//        }
+//        move(speed);
+//    }
+//}
 
 class PlagueFactory extends SimStationFactory {
     public Model makeModel() {
@@ -56,23 +53,104 @@ class PlagueFactory extends SimStationFactory {
 
 }
 
+//class PlagueSimulation extends Simulation {
+//    //public static int VIRULENCE = 50; // % chance of infection
+//    public static int INITIAL_INFECTED = 10;    //Initial %
+//    public void populate() {
+//        //Start with 50 agents
+//
+//        for (int i = 0; i < 50; i++) {
+//            Person newPerson = new Person();
+////            double randomValue = Math.random() * 100;
+////            if(randomValue < VIRULENCE){
+////                newPerson.setInfected(true);
+////            }
+//            addAgent(newPerson);
+//        }
+//
+////        for (Agent agent : this.getList()) {
+////            Plague.Person person = (Plague.Person) agent;
+////            double random = Math.random() * 100;
+////            System.out.println(random);
+////            if (random < VIRULENCE) {
+////                person.setInfected(true);
+////            }
+////        }
+//
+//    }
+//
+//    public void Stats() {
+//        String[] infor = new String[3];
+//        double infectNum = 0;
+//        double infectPercent = 0;
+//        Map<Integer, Integer> countSpeed = new HashMap<>();
+//
+//        //This loop will count the number of infected agents
+//        for (Agent agent : this.getList()) {
+//            Plague.Person person = (Plague.Person) agent;
+//            if (person.isInfected()) {
+//                infectNum++;
+//            }
+//            infectPercent = infectNum / this.getList().size();
+//        }
+//
+//        //Initiate array of String
+//        infor[0] = "agents:" + this.getList().size();
+//        infor[1] = "%infected " + infectPercent * 100;
+//        infor[infor.length - 1] = "clock: " + getClock();
+//        Utilities.inform(infor);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        AppPanel panel = new SimStationPanel(new Plague.PlagueFactory());
+//        panel.display();
+//    }
+//}
+
+class Person extends Agent{
+    public static int RESISTANCE = 2;
+    public static int VIRULENCE = 10;   //Radius
+    private boolean infected;
+    private int resistence;
+
+    public Person(){
+        super();
+        headTo = Heading.random();
+        int randomValue = Utilities.rng.nextInt(100);
+        this.resistence = Utilities.rng.nextInt(RESISTANCE);
+        this.infected = randomValue < PlagueSimulation.INITIAL_INFECTED;
+    }
+
+    public synchronized boolean isInfected(){return this.infected;}
+
+    public synchronized void infect(){
+        if(!this.isInfected()){
+            int randomValue = Utilities.rng.nextInt(100);
+            this.infected = this.resistence < randomValue;
+        }
+    }
+
+    public void update(){
+        if(this.isInfected()){
+            Person neighbor = (Person) this.world.getNeighbor(this, VIRULENCE);
+            if(neighbor != null && !neighbor.isInfected()){
+                neighbor.infect();
+            }
+        }
+        move(1);
+    }
+}
+
 class PlagueSimulation extends Simulation {
-    public static int VIRULENCE = 50; // % chance of infection
+    public static int INITIAL_INFECTED = 10;    //Initial %
 
     public void populate() {
         //Start with 50 agents
+
         for (int i = 0; i < 50; i++) {
-
-            addAgent(new Plague.Person());
+            addAgent(new Person());
         }
-        for (Agent agent : this.getList()) {
-            Plague.Person person = (Plague.Person) agent;
-            if (Math.random() * 100 < VIRULENCE) {
-                person.setInfected(true);
-            }
-        }
-
-
     }
 
     public void Stats() {
@@ -81,20 +159,18 @@ class PlagueSimulation extends Simulation {
         double infectPercent = 0;
         Map<Integer, Integer> countSpeed = new HashMap<>();
 
-
         //This loop will count the number of infected agents
         for (Agent agent : this.getList()) {
             Plague.Person person = (Plague.Person) agent;
             if (person.isInfected()) {
                 infectNum++;
             }
-
             infectPercent = infectNum / this.getList().size();
         }
 
         //Initiate array of String
         infor[0] = "agents:" + this.getList().size();
-        infor[1] = "%infected" + infectPercent * 100;
+        infor[1] = "%infected " + infectPercent * 100;
         infor[infor.length - 1] = "clock: " + getClock();
         Utilities.inform(infor);
     }
